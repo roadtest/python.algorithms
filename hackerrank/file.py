ceb7ec49a1b76095b81f977250f9c4a9d30b4eea
enhong ma <enhong.ma@autodesk.com>
2022-03-06 23:22:46 -0800

    leetcode 200 palindromic string

--- hackerrank/file.py
+++ hackerrank/file.py
@@ -2,8 +2,8 @@
 Output = open ('file2.txt', 'w')
 
 with open('file1.txt', 'r') as Line:
-#    line = Line.readline()
-    for line in Line:
+        line = Line.readline()
+#    for line in Line:
         NewLine = (f"curl -kv  {line}")
      #   Output.write(NewLine + '\n')
         Output.write(NewLine)
--- pytest/test_adder.py
+++
@@ -1,9 +0,0 @@
-# test_adder.py
-#from proj.adder import adder
-from proj.adder import adder
-import unittest
-
-class TestAdder(unittest.TestCase):
-    def test_simple(self):
-        res = adder(2, 3)
-        self.assertEquals(res, 5)
