cdcecc4a4f270acce51f507684f7b5b75075e86f
Carl E. Ma <enhong.ma@autodesk.com>
2022-08-11 17:16:34 -0700

    generator

--- practices/generator.log.file.py
+++ practices/generator.log.file.py
@@ -1,14 +1,20 @@
+"""
 def file_reader(file_path):
     for row in open(file_path, "r"):
         yield row
-
-for row in file_reader('//tmp/1.txt'):
+#when you open, you need close file afterwards
+for row in file_reader("//tmp/1.txt"):
     print(row)
+"""
 
-"""
-with open is using generator, not like open to load file into memory
-with open('path_to_a_large_file') as file:	
-    for row in file:	
-    # do something with each row	
-    pass
-"""
+
+def file_reader(file_path):
+    # with open is using generator, not like open to load file into memory
+    with open(file_path) as file:
+        for row in file:
+            # do something with each row
+            yield row
+
+
+for i in file_reader("//tmp/1.txt"):
+    print(i.strip())
